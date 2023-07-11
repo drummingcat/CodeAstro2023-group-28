a91380b178bfd6efec0d0e6c94a34c7e97493044
drummingcat <126358591+drummingcat@users.noreply.github.com>
2023-07-10 22:22:24 -0600

    Update FirstTestFile.py

--- FirstTestFile.py
+++ FirstTestFile.py
@@ -1,1 +1,21 @@
-print('hello hello hello!')
+import os 
+import numpy as np
+import matplotlib.pylab as plt
+%matplotlib inline
+mat = np.random.random(100,100)
+def matrix_loop(mat, index):
+  index_start = mat.shape[0] #this is where the celestial objects and their stats will be appended
+  index_end = #same thing as before
+#outside of this function, there will be a timestep. This changes the right ascension and declination as the time increases
+#See below for the skew class
+  
+"""
+class skew(object):
+  def __init__(self):
+    altitude = float(input('What is your latitude'))
+    object = str(input('What object would you want to see?'))
+  def calc()
+
+Stellarium has a large database of deep sky objects and stars, and it is all open source. This is what I believe could be used in some way for the data for this program: https://stellarium-web.org
+
+"""
