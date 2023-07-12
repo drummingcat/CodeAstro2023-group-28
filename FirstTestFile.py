3788ba4b8653580c287671b8329b0ffee4f3e923
Cate Davis <catdavis724@gmail.com>
2023-07-12 14:41:55 -0600

    Changes made

--- FirstTestFile.py
+++ FirstTestFile.py
@@ -2,21 +2,24 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-objects = [['Jupiter', '-2.7°', '-234°F', '4.2-6.2 AU', '5.2 AU'], 
-           ['Saturn', 'mag', 'temp', 'distearth', 'distsun'],
-           ['Venus', 'mag', 'temp', 'distearth', 'distsun'],
-           ['Mars', 'mag', 'temp', 'distearth', 'distsun'],
-           ['Mercury', 'mag', 'temp', 'distearth', 'distsun'],
-           ['Neptune', 'mag', 'temp', 'distearth', 'distsun'],
-           ['Uranus', 'mag', 'temp', 'distearth', 'distsun']]
+objects = [['Object: Jupiter', 'Magnitude of -2.7', 'Surface temp of -234°F', 'Between 4.2 and 6.2 AU from earth', '5.2 AU from sun'], 
+           ['Object: Saturn', 'Magnitude of -0.55', 'Surface temp of -288°F', '7 AU from earth', '9.5 AU from sun'],
+           ['Object: Venus', 'Magnitude of -4.6', 'Surface temp of 900°F', 'Around 0.43 AU from earth', '0.72 AU from sun'],
+           ['Object: Mars', 'Magnitude of 1.76', 'Surface temp of between 70° and -225°F', '2.23 AU from Earth', '1.5 AU from the sun'],
+           ['Object: Mercury', 'mag', 'temp', 'distearth', 'distsun'],
+           ['Object: Neptune', 'mag', 'temp', 'distearth', 'distsun'],
+           ['Object: Uranus', 'mag', 'temp', 'distearth', 'distsun']]
 length = (len(objects))
 df = pd.DataFrame(objects, columns=["Objects", "Magnitude", 'Surface Temp','Distance from Earth', 'Distance from sun'])
 ObjectOfChoice = str(input('What is your chosen object?'))
 for i in range (length):
     i += 1
     print(i)
-    if ObjectOfChoice == 'Jupiter':
-        length = 1
-        i = 0
-        Data = pd.Series(objects[i])
-        print(Data)
+if ObjectOfChoice == 'Jupiter':
+    i = 0
+    Data = pd.Series(objects[i])
+    print(Data)
+if ObjectOfChoice == 'Saturn':
+    i = 1
+    Data = pd.Series(objects[i])
+    print(Data)
