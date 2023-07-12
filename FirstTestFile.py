9cb96e4488f57905d97d0447a971b808e389ec1a
Cate Davis <catdavis724@gmail.com>
2023-07-12 13:29:31 -0600

    Made a change

--- FirstTestFile.py
+++ FirstTestFile.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-objects = [['Jupiter', 'mag', 'temp', 'distearth', 'distsun'], 
+objects = [['Jupiter', '-2.7°F', '-234°F', '4.2-6.2 AU', '5.2 AU'], 
            ['Saturn', 'mag', 'temp', 'distearth', 'distsun'],
            ['Venus', 'mag', 'temp', 'distearth', 'distsun'],
            ['Mars', 'mag', 'temp', 'distearth', 'distsun'],
@@ -10,4 +10,10 @@ objects = [['Jupiter', 'mag', 'temp', 'distearth', 'distsun'],
            ['Neptune', 'mag', 'temp', 'distearth', 'distsun'],
            ['Uranus', 'mag', 'temp', 'distearth', 'distsun']]
 df = pd.DataFrame(objects, columns=["Objects", "Magnitude", 'Surface Temp','Distance from Earth', 'Distance from sun'])
-df
+ObjectOfChoice = str(input('What is your chosen object?'))
+for i in range (len(objects)):
+    i += 1
+
+    print(i)
+    Data = pd.Series(objects)
+    print(Data)
